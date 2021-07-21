7d5d5e3213d195608bd1ef22ec026c2dd30d0621
Twinkle <twinkle@Twinkles-MacBook-Pro.local>
2021-07-21 21:08:46 +0530

    Cloned from Corey Schafer Django Tutorial

---
+++ users/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    # class Meta gives us a nested namespace for the configurations and keeps the configurations in one place.
+
+    # within the configuration we are saying: we want to affect the usermodel and we want the following fields in that order.
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
